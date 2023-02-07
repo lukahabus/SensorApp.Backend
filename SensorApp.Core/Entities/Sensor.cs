27a4076e97d84150978e8a593dce9142cf9a6d71
Luka Habuš <habusluka@gmail.com>
2023-02-07 13:55:45 +0100

    DbContext

---
+++ SensorApp.Core/Entities/Sensor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorApp.Core.Entities
+{
+    internal class Sensor
+    {
+        public float Humidity { get; set; }
+
+        public float Light { get; set; }
+
+        public float Temperature { get; set; }
+    }
+}
---
+++ SensorApp.Infrastructure/SensorAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorApp.Infrastructure
+{
+    public class SensorAppDbContext : DbContext
+    {
+        public SensorAppDbContext(DbContextOptions options) : base(options) { }
+
+        
+    }
+}
